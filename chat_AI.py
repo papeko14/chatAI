aecca5f5d05085086256d39bf66402ed62c368d2
thanopath <44945805+papeko14@users.noreply.github.com>
2025-08-23 10:38:26 +0700

    Update chat_AI.py

--- chat_AI.py
+++ chat_AI.py
@@ -10,11 +10,12 @@ st.set_page_config(page_title="n8n Chatbot App", layout="centered")
 Data = st.Page(page='Table_Data.py',title='Data', icon='📅')
 chat_bot = st.Page(page='Chat_page.py',title='Chat_bot', icon='🤖')
 chat_zone = st.Page(page='Chat_page_zone.py',title='Chat_bot_zone',icon='🤖')
-graph = st.Page(page=r'Graph_data.py',title='Chat_bot', icon='📊')
+graph = st.Page(page=r'Graph_data.py',title='Data Visualization', icon='📊')
 pg = st.navigation(
         pages=[chat_bot,chat_zone,Data,graph]
     )
 pg.run()
+
 
 
 
