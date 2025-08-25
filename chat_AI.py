011f29c54fe78af1297d58f9aa6720815f464292
thanopath <44945805+papeko14@users.noreply.github.com>
2025-08-25 21:11:34 +0700

    Update chat_AI.py

--- chat_AI.py
+++ chat_AI.py
@@ -1,23 +1,20 @@
 import streamlit as st
 import json
 import os
-
 # --- Page Configurations ---
 
 st.set_page_config(page_title="n8n Chatbot App", layout="centered")
 # --- Sidebar for Navigation ---
 # สร้างเมนูทางด้านซ้าย (Sidebar)
-Data = st.Page(page='Table_Data.py',title='Data', icon='📅')
-chat_bot = st.Page(page='Chat_page.py',title='Chat_bot', icon='🤖')
-chat_zone = st.Page(page='Chat_page_zone.py',title='Chat_bot_zone',icon='🤖')
-graph = st.Page(page=r'Graph_data.py',title='Data Visualization', icon='📊')
-test_db = st.Page(page=r'test_db.py',title='Data base', icon='📊')
-
+Data = st.Page(page=r'C:\Users\papeko14\Documents\fastwork\สอนไพธอน\mentor\2025-07-29\views\testdata.py',title='Datatest', icon='📅')
+chat_bot = st.Page(page=r'C:\Users\papeko14\Documents\fastwork\สอนไพธอน\mentor\2025-07-29\views\Chat_page.py',title='Chat_bot', icon='🤖')
+chat_zone = st.Page(page=r'C:\Users\papeko14\Documents\fastwork\สอนไพธอน\mentor\2025-07-29\views\Chat_page_zone.py',title='Chat_bot_zone',icon='🤖')
+graph = st.Page(page=r'C:\Users\papeko14\Documents\fastwork\สอนไพธอน\mentor\2025-07-29\views\Graph_data.py',title='graph', icon='📊')
+test_data = st.Page(page=r'C:\Users\papeko14\Documents\fastwork\สอนไพธอน\mentor\2025-07-29\views\Table_Data.py',title='show_db')
+pg = st.navigation(
+        pages=[chat_bot,Data,graph,chat_zone,test_data]
+    )
 pg.run()
-
-
-
-
 
 
 
